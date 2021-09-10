540afcbe79592c74cc00e491ec297aee5bbd74dc
sumanmaji16 <88018961+sumanmaji16@users.noreply.github.com>
2021-09-11 02:22:03 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,4 @@
+name = input("Enter your name : ")
+age = int (input ("Enter your age : "))
+year = str ( ( 2021 - age ) + 100 )
+print ("Hi: " + name + ", you will become 100 years in the year " + year )
---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,6 @@
+num = int (input ("Please enter your number : "))
+num2 = num%2
+if num2 == 0 :
+    print ("The entered number is an even number")
+else :
+    print ("The entered number is an odd number")    
