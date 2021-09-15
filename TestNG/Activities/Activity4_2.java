e552ba74ed854a03fd64b191d2df310283ff4455
sumanmaji16 <88018961+sumanmaji16@users.noreply.github.com>
2021-09-16 00:59:16 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity4_2.java
@@ -0,0 +1,10 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class Activity4_2  {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
